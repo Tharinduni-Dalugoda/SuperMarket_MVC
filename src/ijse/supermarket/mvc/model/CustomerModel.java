9cd6eb483fcbfc77ee112603fc148ccabbfd445a
Tharinduni <tharindunilakshani.9887@gmail.com>
2024-06-29 21:34:10 +0530

    save customer

--- src/ijse/supermarket/mvc/model/CustomerModel.java
+++ src/ijse/supermarket/mvc/model/CustomerModel.java
@@ -4,10 +4,40 @@
  */
 package ijse.supermarket.mvc.model;
 
+
+import java.sql.Connection;
+import ijse.supermarket.mvc.db.DBconnection;
+import ijse.supermarket.mvc.dto.CustomerDto;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
+
 /**
  *
  * @author user
  */
 public class CustomerModel {
+    //save customer
+    public String saveCustomer(CustomerDto customerDto) throws ClassNotFoundException, SQLException{
+        Connection connection = DBconnection.getinstance().getConnection();
+        
+        String sql = "INSERT INTO Customer VALUES (?,?,?,?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        
+        statement.setString(1,customerDto.getCustID());
+        statement.setString(2,customerDto.getCustName());
+        statement.setString(3,customerDto.getDOB());
+        statement.setString(4,customerDto.getAddress());
+        statement.setInt(5,customerDto.getSalary());
+        statement.setInt(6,customerDto.getTelNo());
+        
+        if(statement.executeUpdate()>0){
+            return "Success";     
+        }else{
+            return "fail";
+        }
+        
+                
+        }
     
 }
